b2736dbb7608924e9c89ff1b8bbb8517848d50b5
Guan-Lin Li <youmu257@gmail.com>
2020-03-26 10:03:00 +0800

    refactor: Refactor send mail function (#15)

--- scripts/mailjet.py
+++ scripts/mailjet.py
@@ -7,7 +7,20 @@ Do not forget to update the sender address used in the sample
 
 from mailjet_rest import Client
 import configparser
-from util import ConfigSectionMap
+from util import configSectionMap
+from Enum.subscribeType import SubscribeType
+from database.mysqlConnect import *
+from database.model.subscribers import *
+from database.model.pornVideo import *
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+configMap = configSectionMap("MAILJET", config)
+
+apiKey = configMap['mj_apikey_public']
+apiSecret = configMap['mj_apikey_private']
+fromMail = configMap['from_email_address']
+mailTitle = configMap['mail_title']
 
 
 # print the success or failure of your calls.
@@ -33,55 +46,54 @@ def checkStatusCode(result):
         print('Unknown status code message')
 
 
-def sendMail():
-    config = configparser.ConfigParser()
-    config.read("config.ini")
-    configMap = ConfigSectionMap("MAILJET", config)
-
-    apiKey = configMap['mj_apikey_public']
-    apiSecret = configMap['mj_apikey_private']
-    fromMail = configMap['from_email_address']
-
-    mailTitle = 'Your email flight plan!2'
-    fromName = 'TodayCo'
-    contentText = (
-        'Dear passenger, welcome to Mailjet!' +
-        'May the delivery force be with you!'
-    )
-    contentHtml = (
-        '<h3>Dear passenger, welcome to Mailjet!</h3>' +
-        '<br />May the delivery force be with you!'
-    )
-    recipients = list()
-
-    recipients.append({"Email": "youmu257@gmail.com"})
-    recipients.append({"Email": "ragnro_456@yahoo.com.tw"})
-
-    print(apiKey)
-    print(apiSecret)
-    print(fromMail)
-    print(recipients)
+def sendMail(recipients, mailContent):
+    global apiKey, apiSecret, fromMail
 
     mailjetTest = Client(auth=(apiKey, apiSecret))
     data = {
-      'FromEmail': fromMail,
-      'FromName': fromName,
-      'Subject': mailTitle,
-      'Text-part': contentText,
-      'Html-part': contentHtml,
-      'Recipients': recipients
+        'FromEmail': fromMail,
+        'FromName': 'TodayCo',
+        'Subject': mailTitle,
+        # 'Text-part': contentText,
+        'Html-part': mailContent,
+        'Recipients': recipients
     }
 
+    result = mailjetTest.send.create(data=data)
+    checkStatusCode(result)
 
-recipients = list()
-recipients.append({"Email": "youmu257@gmail.com"})
-contentText = (
-    'Dear passenger, welcome to Mailjet!' +
-    'May the delivery force be with you!'
-)
-contentHtml = (
-    '<h3>Dear passenger, welcome to Mailjet!</h3>' +
-    '<br />May the delivery force be with you!'
-)
-# sentMail(recipients, contentText, contentHtml)
-sentMailTest()
+
+def getTopWeeklyMailContent():
+    pornVideoList = getTopWeeklyPornVideos(10)
+    content = 'This is weekly top 10 video list.<br>'
+    for row in pornVideoList:
+        content += row.video_title+'<br>'
+    return content
+
+
+def getTopDailyMailContent():
+    pornVideoList = getTopWeeklyPornVideos(10)
+    content = 'This is daily top 10 video list.<br>'
+    for row in pornVideoList:
+        content += row.video_title+'<br>'
+    return content
+
+
+# initialize MySQL database connection
+connectMysql()
+
+recipients = {SubscribeType.WEEKLY.value: [], SubscribeType.DAILY.value: []}
+
+# get subscribers email list
+emails = getVerifySubscribers()
+
+for row in emails:
+    recipients[row.type].append({"Email": row.email})
+
+# send mail to weekly subscribers
+if recipients[SubscribeType.WEEKLY.value]:
+    sendMail(recipients[SubscribeType.WEEKLY.value], getTopWeeklyMailContent())
+
+# send mail to daily subscribers
+if recipients[SubscribeType.DAILY.value]:
+    sendMail(recipients[SubscribeType.DAILY.value], getTopDailyMailContent())
